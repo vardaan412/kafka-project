6442eb96dbfdd81d4f18ec66988eced2933c45b7
vardaan412 <Saxenavardaan18@gmail.com>
2025-02-27 14:14:48 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,205 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# VPC
+resource "aws_vpc" "kafka_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "kafka-vpc"
+  }
+}
+
+# Public Subnet
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.kafka_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+  tags = {
+    Name = "public-subnet-1"
+  }
+}
+
+# Private Subnets
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.kafka_vpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "private-subnet-1"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.kafka_vpc.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "private-subnet-2"
+  }
+}
+
+# Internet Gateway for Public Subnet
+resource "aws_internet_gateway" "kafka_igw" {
+  vpc_id = aws_vpc.kafka_vpc.id
+  tags = {
+    Name = "kafka-igw"
+  }
+}
+
+# Public Route Table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.kafka_vpc.id
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.kafka_igw.id
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# NAT Gateway for Private Subnets
+resource "aws_eip" "nat_eip" {}
+
+resource "aws_nat_gateway" "kafka_nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet_1.id
+  tags = {
+    Name = "kafka-nat-gateway"
+  }
+}
+
+# Private Route Table
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.kafka_vpc.id
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+resource "aws_route" "private_nat_access" {
+  route_table_id         = aws_route_table.private_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.kafka_nat.id
+}
+
+resource "aws_route_table_association" "private_assoc_1" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_assoc_2" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+# Security Groups
+resource "aws_security_group" "bastion_sg" {
+  vpc_id = aws_vpc.kafka_vpc.id
+  tags = {
+    Name = "bastion-sg"
+  }
+
+  # Allow SSH from your public IP
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["49.36.179.31/32"]
+  }
+
+  # Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "private_sg" {
+  vpc_id = aws_vpc.kafka_vpc.id
+  tags = {
+    Name = "private-sg"
+  }
+
+  # Allow SSH from Bastion Host
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion_sg.id]
+  }
+
+  # Allow Kafka traffic
+  ingress {
+    from_port   = 9092
+    to_port     = 9092
+    protocol    = "tcp"
+    security_groups = [aws_security_group.bastion_sg.id]
+  }
+
+  # Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instances
+resource "aws_instance" "bastion" {
+  ami                    = "ami-04b4f1a9cf54c11d0"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public_subnet_1.id
+  vpc_security_group_ids = [aws_security_group.bastion_sg.id]
+  key_name               = "CommonKey"
+  tags = {
+    Name = "bastion-instance"
+  }
+}
+
+resource "aws_instance" "private_instance_1" {
+  ami                    = "ami-04b4f1a9cf54c11d0"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.private_subnet_1.id
+  vpc_security_group_ids = [aws_security_group.private_sg.id]
+  key_name               = "CommonKey"
+  tags = {
+    Name = "kafka-instance-1"
+  }
+}
+
+resource "aws_instance" "private_instance_2" {
+  ami                    = "ami-04b4f1a9cf54c11d0"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.private_subnet_2.id
+  vpc_security_group_ids = [aws_security_group.private_sg.id]
+  key_name               = "CommonKey"
+  tags = {
+    Name = "kafka-instance-2"
+  }
+}
+
+# Outputs
+output "bastion_public_ip" {
+  value = aws_instance.bastion.public_ip
+}
+
+output "private_instance_1_ip" {
+  value = aws_instance.private_instance_1.private_ip
+}
+
+output "private_instance_2_ip" {
+  value = aws_instance.private_instance_2.private_ip
+}

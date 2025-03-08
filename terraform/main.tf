834a245f55a04153a2c1e72bae142a80a70100c2
vardaan412 <Saxenavardaan18@gmail.com>
2025-03-08 13:22:01 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -106,7 +106,7 @@ resource "aws_security_group" "bastion_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["YOUR_IP/32"] # Replace with your own IP for security
+    cidr_blocks = ["0.0.0.0/0"] 
   }
 
   egress {

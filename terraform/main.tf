705b5645c9212f5a9673ff87f582bee315f3fe59
vardaan412 <Saxenavardaan18@gmail.com>
2025-03-08 15:51:48 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -141,6 +141,7 @@ resource "aws_security_group" "private_sg" {
     to_port         = 22
     protocol        = "tcp"
     security_groups = [aws_security_group.bastion_sg.id]
+    cidr_blocks     = ["10.0.0.0/0"]
   }
 
   # Allow Kafka traffic
@@ -149,6 +150,7 @@ resource "aws_security_group" "private_sg" {
     to_port     = 9092
     protocol    = "tcp"
     security_groups = [aws_security_group.bastion_sg.id]
+    cidr_blocks     = ["10.0.0.0/0"]
   }
 
   # Allow Zookeeper traffic
@@ -157,6 +159,7 @@ resource "aws_security_group" "private_sg" {
     to_port     = 2181
     protocol    = "tcp"
     security_groups = [aws_security_group.bastion_sg.id]
+    cidr_blocks     = ["10.0.0.0/0"]
   }
 
   # Allow all outbound traffic

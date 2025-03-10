715787e7daa5c76d1816cac4485a1786999b48d5
vardaan412 <Saxenavardaan18@gmail.com>
2025-03-10 16:57:24 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,22 +3,22 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "./modules/vpc"
+  source = "./module/vpc"
 }
 
 module "security_groups" {
-  source = "./modules/security_groups"
+  source = "./module/security_groups"
   vpc_id = module.vpc.vpc_id
 }
 
 module "vpc_peering" {
-  source                 = "./modules/vpc_peering"
+  source                 = "./module/vpc_peering"
   vpc_id                 = module.vpc.vpc_id
   private_route_table_id = module.vpc.private_route_table_id
 }
 
 module "ec2" {
-  source               = "./modules/ec2"
+  source               = "./module/ec2"
   public_subnet_id     = module.vpc.public_subnet_id
   private_subnet_1_id  = module.vpc.private_subnet_1_id
   private_subnet_2_id  = module.vpc.private_subnet_2_id
